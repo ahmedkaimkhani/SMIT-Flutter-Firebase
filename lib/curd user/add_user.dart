86a1522aa92fd1336bc50da1fa2b1faecac3ee42
ahmedkaimkhani <ak9302359@gmail.com>
2023-10-18 03:05:47 +0500

    Firebase

--- lib/curd user/add_user.dart
+++ lib/curd user/add_user.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_smit_flutter/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class CurdAddUsers extends StatefulWidget {
@@ -12,11 +15,34 @@ class _CurdAddUsersState extends State<CurdAddUsers> {
   addUsers() {
     FirebaseFirestore.instance
         .collection('users')
-        .add({'name': 'Ahmed', 'contact': '031571863'});
+        .add({'name': 'Ahmed', 'contact': '031571863'})
+        .then((value) => Utils().toastMessage('User data added successfully'))
+        .onError((error, stackTrace) => Utils().toastMessage('$e'));
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addUsers();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot.data!.docs.toString());
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return const ListTile();
+              },
+            );
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
   }
 }
